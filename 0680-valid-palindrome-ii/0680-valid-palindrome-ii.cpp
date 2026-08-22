6580106127c59ab0034a456cb3be2c50d817fbaa
anujks20 <rikkisharma1528@gmail.com>
2026-08-22 19:59:39 +0530

    Time: 8 ms (20.96%), Space: 26.6 MB (32.87%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            }
+            else{
+                return (isPal(s, l + 1, r) || isPal(s, l, r - 1));
+            }
+        }
+        return true;
+    }
+    bool isPal(string s, int l, int r){
+        while (l < r){
+            if (s[l] != s[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
